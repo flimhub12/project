f74141d2ab436a13020e7408aa5d639be8f5d9a2
Brijesh yadav <recs.cse17903@gmail.com>
2022-10-26 16:31:22 +0530

    Update If.java

--- src/If.java
+++ src/If.java
@@ -5,7 +5,7 @@ public class If {
     public static void main(String args[]){
 
         Scanner N = new Scanner(System.in);
-          int n = N.nextInt();
+          int number = N.nextInt();
         if (90<=n){
             System.out.println("A");
         }
