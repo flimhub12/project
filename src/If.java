98513774171fd9581ac0cb9b728f25dc32b9b7db
flimhub12 <116723350+flimhub12@users.noreply.github.com>
2022-10-26 16:28:45 +0530

    Add files via upload

---
+++ src/If.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class If {
+
+    public static void main(String args[]){
+
+        Scanner N = new Scanner(System.in);
+          int n = N.nextInt();
+        if (90<=n){
+            System.out.println("A");
+        }
+        else {
+            System.out.println("test");
+        }
+
+    }
+}
---
+++ src/LinearSearch.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String args[]) {
+
+        int value;
+
+        int[] array = {10, 5, 21, 16, 11};
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the searched value");
+        value = input.nextInt();
+        boolean isValueFound = false;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                isValueFound = true;
+                index = i;
+
+            }
+
+        }
+
+        if (isValueFound) {
+            System.out.println(value + " is found at Index " + (index));
+        } else {
+            System.out.println(value + " is not in the list");
+        }
+
+
+    }
+}
